b827d7fdea267ef2935dff96043a38150881d8a6
Nielson Rolim <nielson.rolim@gmail.com>
2014-08-18 22:34:55 -0300

    n

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -6,6 +6,14 @@ class Indicator < ActiveRecord::Base
 
   validates_presence_of :name, :indicator_category
 
+  def has_obs_for_visit?(visit_id)
+    unless self.obs(visit_id).nil?
+      return !self.obs(visit_id).empty?
+    else
+      return false
+    end
+  end
+
   def obs(visit_id)
     IndicatorVisit.where('indicator_id = ? and visit_id = ?', self.id, visit_id).first.obs
   end
