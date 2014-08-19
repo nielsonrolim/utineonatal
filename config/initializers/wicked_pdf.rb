b74b8eabc95fc1d204b97a48f28c72f3390ce8a2
Nielson Rolim <nielson.rolim@gmail.com>
2014-08-19 08:18:47 -0300

    wicked_pdf config fixed

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,14 +1,6 @@
-platform = RUBY_PLATFORM
-if platform.include?("darwin") # OS X machine
-  binary_path = '/Users/nielsonrolim/.rvm/gems/ruby-2.1.2@utineonatal/bin/wkhtmltopdf'
-elsif platform.include?("64-linux") # 64-bit linux machine
-  binary_path = '/home/garimpo/.rvm/gems/ruby-2.1.2@utineonatal/bin/wkhtmltopdf'
-end
-
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   :layout => "pdf.pdf.erb",
 
   #:exe_path => '/usr/local/bin/wkhtmltopdf'
-  :exe_path => binary_path
 }
