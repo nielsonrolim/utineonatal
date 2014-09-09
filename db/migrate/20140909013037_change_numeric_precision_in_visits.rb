e3a4607a7b395e892cba0e12410509a1c3cdc029
Nielson Rolim <nielson.rolim@gmail.com>
2014-09-08 22:48:58 -0300

    bug fixed

--- db/migrate/20140909013037_change_numeric_precision_in_visits.rb
+++
@@ -1,9 +0,0 @@
-class ChangeNumericPrecisionInVisits < ActiveRecord::Migration
-  def change
-  	change_column :visits, :weight, :decimal, precision: 4, scale: 2
-  	change_column :visits, :stature, :decimal, precision: 4, scale: 2
-  	change_column :visits, :head_circumference, :decimal, precision: 4, scale: 2
-  	change_column :visits, :thoracic_perimeter, :decimal, precision: 4, scale: 2
-  	change_column :visits, :temperature, :decimal, precision: 4, scale: 2
-  end
-end
